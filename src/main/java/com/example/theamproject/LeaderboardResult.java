e03de6a91149f4bcf54afe4758e3e0552506390f
홍정민 <s01046927968@gmail.com>
2026-06-14 02:37:52 +0900

    feat: 어드민 기능 추가

--- src/main/java/com/example/theamproject/LeaderboardResult.java
+++ src/main/java/com/example/theamproject/LeaderboardResult.java
@@ -3,13 +3,13 @@ package com.example.theamproject;
 import java.util.List;
 
 public class LeaderboardResult {
-    // 유니티 변수명과 매칭하기 편하도록 랭킹 리스트를 감싸는 필드
-    private List<LeaderboardResponse> skiers; // 혹은 ranking, players 등 원하는 이름
+    private List<LeaderboardResponse> skiers;
 
     public LeaderboardResult(List<LeaderboardResponse> skiers) {
         this.skiers = skiers;
     }
 
+    // ⭐ 여기도 Getter 필수!
     public List<LeaderboardResponse> getSkiers() {
         return skiers;
     }
